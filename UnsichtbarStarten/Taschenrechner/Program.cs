f7dd2f6fbf3b35f11d83bcb70d00a18ba729b024
Michael Zöhling <michael.zoehling@outlook.com>
2019-06-14 11:41:27 +0200

    Formular unsichtbar starten lassen

---
+++ UnsichtbarStarten/Taschenrechner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] arg) // argumente in der Konsole
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new Form1();
+
+            if (arg.Length > 0 && arg[0] == "unsichtbar")
+            {
+                form.Visible = false;
+                form.WindowState = FormWindowState.Minimized;
+                form.ShowInTaskbar = false;
+            }
+
+            Application.Run(form);
+        }
+    }
+}
